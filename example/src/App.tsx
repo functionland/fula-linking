9c8f1b42058f1a1427d0f5d1a5845a280dc8f7f7
ehsan shariati <ehsan6sha@gmail.com>
2022-11-08 21:33:58 -0500

    Initial commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,133 @@
-import * as React from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useState, useEffect } from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'rn-fula-linking';
+import { StyleSheet, View, Button, Text, BackHandler } from 'react-native';
+import { encodeIdentity } from 'rn-fula-linking';
+import SvgQRCode from 'react-native-qrcode-svg';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { decodeIdentity } from '../../.history/src/index_20221108200539';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [qrCode, setQrCode] = useState<string | undefined>();
+  const [mode, setMode] = useState<'login' | 'add' | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+  const [hasPermission, setHasPermission] = useState<Boolean | undefined>();
+  const [scannedIdentity, setScannedIdentity] = useState<any>();
+
+  const backAction = () => {
+    setMode(undefined);
+    setScannedIdentity(undefined);
+    return true;
+  };
+
+  //We wait for the application to load
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', backAction);
+
+    //We encode the identity of type { did: string; peerId: PeerId } to create a string from it
+    //The encode function returns a Promise
+    encodeIdentity({ did: 'testdid', peerId: 'testpeerid' }).then(
+      (response) => {
+        setQrCode(response);
+      }
+    );
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', backAction);
+    };
   }, []);
 
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+    if (mode === 'add') {
+      getBarCodeScannerPermissions();
+    }
+  }, [mode]);
+
+  function showQrCode() {
+    setMode('login');
+  }
+
+  function showScanner() {
+    setMode('add');
+  }
+
+  //Here we give the result of scanned barcode to decodeIdentity to give us the did and peerId in json format
+  const handleBarCodeScanned = ({ type, data }) => {
+    decodeIdentity(data)
+      .then((identity) => {
+        console.log(identity);
+        setScannedIdentity(identity);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View
+      style={[
+        styles.container,
+        {
+          flexDirection: 'column',
+        },
+      ]}
+    >
+      {mode === undefined && (
+        <View
+          style={[
+            styles.container,
+            {
+              flexDirection: 'row',
+            },
+          ]}
+        >
+          <View style={{ flex: 1, padding: 2 }}>
+            <Button
+              onPress={showQrCode}
+              title="Login"
+              color="#841584"
+              accessibilityLabel="Login on this device with a current identity"
+            />
+          </View>
+          <View style={{ flex: 2 }}>
+            <Button
+              onPress={showScanner}
+              title="Add Another Device"
+              color="#841584"
+              accessibilityLabel="Add another device to the current identity"
+            />
+          </View>
+        </View>
+      )}
+      {mode === 'login' && <SvgQRCode value={qrCode} />}
+      {mode === 'add' && (
+        <BarCodeScanner
+          onBarCodeScanned={scannedIdentity ? undefined : handleBarCodeScanned}
+          style={[StyleSheet.absoluteFillObject]}
+        />
+      )}
+      {scannedIdentity && scannedIdentity.did && (
+        <View>
+          <Text style={{ color: 'white' }}>
+            did = {scannedIdentity.did} {'\n'}
+            peerId = {scannedIdentity.peerId}
+          </Text>
+          <Button
+            title={'Tap to Scan Again'}
+            onPress={() => setScannedIdentity(false)}
+          />
+        </View>
+      )}
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,30 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import type PeerId from '@libp2p/interface-peer-id';
+
+export function encodeIdentity(decodedIdentity: {
+  did: string;
+  peerId: PeerId;
+}): Promise<string> {
+  let encodedIdentity = JSON.stringify({
+    did: decodedIdentity.did,
+    peerId: decodedIdentity.peerId,
+  });
+  return Promise.resolve(encodedIdentity);
+}
+
+export function decodeIdentity(
+  encodedIdentity: string
+): Promise<{ did: string; peerId: PeerId } | string> {
+  try {
+    let decodedIdentity = JSON.parse(encodedIdentity);
+    return Promise.resolve(decodedIdentity);
+  } catch (e) {
+    let error = '';
+    if (e instanceof Error) {
+      error = e.message;
+    } else {
+      error = String(e);
+    }
+    Promise.reject(error);
+  }
+  return Promise.resolve('');
 }
